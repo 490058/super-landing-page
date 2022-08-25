d00661c74efc2ceb9bbdd38837dc65a2acc2667f
490058 <490058@gmail.com>
2022-08-25 11:12:41 -0400

    add page title

--- main.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementsByClassName("header").style.height = "9px";
-  } else {
-    document.getElementsByClassName("header").style.fontSize = "9px";
-  }
-}
